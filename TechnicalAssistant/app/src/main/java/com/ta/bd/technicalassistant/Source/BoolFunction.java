213860c0390c6e0ea3de970925e952d8f4937ab1
Vitaliy Bonar <bonarv96@gmail.com>
2015-05-01 14:46:02 +0300

    Fixed code style

--- TechnicalAssistant/app/src/main/java/com/ta/bd/technicalassistant/Source/BoolFunction.java
+++ TechnicalAssistant/app/src/main/java/com/ta/bd/technicalassistant/Source/BoolFunction.java
@@ -4,25 +4,25 @@ package com.ta.bd.technicalassistant.Source;
  * Created by Vitaliy on 4/28/2015.
  */
 class BoolFunction {
-    private String[] truthTable;
+    private String[] truth_table;
 
     public String[] getTruthTable() {
-        return truthTable;
+        return truth_table;
     }
 
 
 
     BoolFunction(String function) {
-        truthTable = new String [function.length()];
-        for(int i = 0; i < truthTable.length; i++)
-            truthTable[i] = "";
+        truth_table = new String [function.length()];
+        for(int i = 0; i < truth_table.length; i++)
+            truth_table[i] = "";
         for(int i = 0, divider = function.length() / 2; i < Math.log(function.length()) / Math.log(2); i++, divider /= 2)
             for(int j = 0; j < function.length(); j++)
                 if(((j / divider) & 1) == 0)
-                    truthTable[j] += '0';
+                    truth_table[j] += '0';
                 else
-                    truthTable[j] += '1';
+                    truth_table[j] += '1';
         for(int i = 0; i < function.length(); i++)
-            truthTable[i] += function.charAt(i);
+            truth_table[i] += function.charAt(i);
     }
 }
